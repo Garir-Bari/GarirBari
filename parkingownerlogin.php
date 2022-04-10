925c75534b944f11121d8c34b08e5b01662907c8
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-10 08:00:55 +0600

    Parking owner login process page is created

--- parkingownerlogin.php
+++ parkingownerlogin.php
@@ -66,12 +66,12 @@
                   <div class="col-xl-6 col-xxl-6 col-md-12 col-sm-12 row d-flex justify-content-center">
                         <div class="card ">
                               <h2 class="text-center">Login As Parking Owner</h2>
-                              <form action="#">
+                              <form action="ParkingOwnerLoginProcess.php" method="POST">
                                 <div class="inputBox">
-                                  <input type="text" placeholder="Username">
+                                  <input type="text" placeholder="UserEmail" name="email">
                                 </div>
                                 <div class="inputBox">
-                                  <input type="password" placeholder="Password">
+                                  <input type="password" placeholder="Password" name="password">
                                 </div>
                                 <div class="inputBox text-center">
                                     <button class="btn ">Submit</button>
