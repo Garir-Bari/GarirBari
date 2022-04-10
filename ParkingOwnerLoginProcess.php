36606d49c32883eeaee416fd061df528c820e92a
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-10 08:18:48 +0600

    redirect to parking owner home after login

--- ParkingOwnerLoginProcess.php
+++ ParkingOwnerLoginProcess.php
@@ -7,28 +7,37 @@
     ini_set('display_startup_errors', 0);
     error_reporting(0);
 
-    $query = "SELECT * FROM `parking_owner` WHERE email = '$email' AND pass  = '$password';";
-    $create_query = mysqli_query($conn,$query);
-    $result2 = mysqli_fetch_array($create_query);
-    $count=mysqli_num_rows($create_query);
+    $query = "SELECT * FROM `parking_owner` WHERE email = '$email' AND pass  = '$pass';";
+    $execute_query = mysqli_query($conn,$query);
+    //$result2 = mysqli_fetch_array($execute_query);
+    $count=mysqli_num_rows($execute_query);
 
-  if ($count > 0 && $result2['status']=='pending'){
-		session_start();
-		$_SESSION['job_seeker_id']=$result2['job_seeker_id'];
-		header('location: ../skill_test_varrified/index.php');
-	}else if ($count > 0 && $result2['status']=='approved'){
-		session_start();
-		$_SESSION['job_seeker_id']=$result2['job_seeker_id'];
-		header('location: ../job_seeker_Home/index.php');
-	 }
-  //else if($username != $result2['username'] && $password != $result2['pass']){
-	// 	header('location:login_error.php?username=error&pass=error');
-	// }else if($username != $result2['username']){
-	// 	header('location:login_error.php?username=error');
-	// }else if($password != $result2['pass']){
-	// 	header('location:login_error.php?pass=error');
-  // }else{
-	// 	header('location:login_error.php?username=error&pass=error');
-  // }
+    if ($count > 0){
+        //header('home.html');
+        ?>
+        <script>
+            window.location.assign('home.html');
+        </script>
+        <?php
+
+    }else{
+        echo 'unsuccessfull';
+        //header('parkingownerlogin.php');
+        ?>
+        <script>
+            window.location.assign('parkingownerlogin.php');
+        </script>
+        <?php
+    }
+
+//   if ($count > 0 && $result2['status']=='pending'){
+// 		session_start();
+// 		$_SESSION['job_seeker_id']=$result2['job_seeker_id'];
+// 		header('location: ../skill_test_varrified/index.php');
+// 	}else if ($count > 0 && $result2['status']=='approved'){
+// 		session_start();
+// 		$_SESSION['job_seeker_id']=$result2['job_seeker_id'];
+// 		header('location: ../job_seeker_Home/index.php');
+// 	 }
 
 ?>
