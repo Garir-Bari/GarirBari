b3ce6d7beafbe0ab031797c330f2327b1ed83721
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 01:17:00 +0600

    query ececuted for parking owner account approval

--- POapprove.php
+++ POapprove.php
@@ -2,7 +2,7 @@
     require_once('db_connect.php');
     $id = $_GET['id'];
     $queryapprove = "UPDATE parking_owner SET status = 'approved' WHERE id = $id;";
-    //$getData = mysqli_query($conn,$queryapprove);
-    //header("Location: index.php");
+    $approveExecute = mysqli_query($conn,$queryapprove);
+    header("Location: index.php");
 
 ?>
