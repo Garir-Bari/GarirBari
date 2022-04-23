158b096c902720549868c99383443145412cd9e2
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 01:45:55 +0600

    DB is connected

---
+++ POreject.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('db_connect.php');
+    //$id = $_GET['id'];
+    //$queryapprove = "UPDATE parking_owner SET status = 'approved' WHERE id = $id;";
+    //$approveExecute = mysqli_query($conn,$queryapprove);
+    //header("Location: admin.php");
+
+?>
