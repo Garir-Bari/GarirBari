c591d3e53de963c0e1c17f88420bef3457feb780
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 01:13:07 +0600

    DB is connected

---
+++ POapprove.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('db_connect.php');
+    //$id = $_GET['id'];
+    //$queryapprove = "UPDATE job_recruiter SET status = 'approved' WHERE recruiter_id = $id;";
+    //$getData = mysqli_query($conn,$queryapprove);
+    //header("Location: index.php");
+
+?>
