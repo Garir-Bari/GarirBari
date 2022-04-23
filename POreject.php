e41ae8b6a714081ba3b07df7be8845e4387fc36b
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 01:49:21 +0600

    Query written & executed for accout approval rejection

--- POreject.php
+++ POreject.php
@@ -1,7 +1,7 @@
 <?php
     require_once('db_connect.php');
     $id = $_GET['id'];
-    //$queryapprove = "UPDATE parking_owner SET status = 'approved' WHERE id = $id;";
+    $queryReject = "DELETE FROM parking_owner WHERE id = $id;";
     //$approveExecute = mysqli_query($conn,$queryapprove);
     //header("Location: admin.php");
 
