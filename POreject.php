22591580569557eb7747c62711b3aee26561db0f
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 01:50:49 +0600

    after Query execution redirected to admin home

--- POreject.php
+++ POreject.php
@@ -2,7 +2,7 @@
     require_once('db_connect.php');
     $id = $_GET['id'];
     $queryReject = "DELETE FROM parking_owner WHERE id = $id;";
-    //$approveExecute = mysqli_query($conn,$queryapprove);
-    //header("Location: admin.php");
+    $rejectExecute = mysqli_query($conn,$queryReject);
+    header("Location: admin.php");
 
 ?>
