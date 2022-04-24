e04ee1ea7754aad7a30655fb028b3d2c5f538e14
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 12:32:22 +0600

    delete query written

--- PGdelete.php
+++ PGdelete.php
@@ -1,7 +1,7 @@
 <?php
     require_once('db_connect.php');
     $id = $_GET['id'];
-    //$queryapprove = "UPDATE parking_garage SET status = 'approved' WHERE id = $id;";
+    $queryapprove = "DELETE FROM parking_garage WHERE id = $id;";
     //$approveExecute = mysqli_query($conn,$queryapprove);
     //header("Location: admin.php");
 
