ee931e2a41cffade98e39efac6cb0556db5563ff
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 12:54:55 +0600

    info added with update button

--- POHome.php
+++ POHome.php
@@ -178,7 +178,7 @@
                                         <td class="column6"><?php echo $cctv; ?></td>
                                         <td class="column6"><?php echo $status; ?></td>
                                         <?php
-                                            echo "<td> <a href = 'PGupdate.php?id=$id&c_name=$c_name&c_nid=$c_nid'> Update </a> </td>";
+                                            echo "<td> <a href = 'PGupdate.php?id=$id&c_name=$c_name&c_nid=$c_nid&contact=$contact&p_address=$p_address&no_of_spot=$no_of_spot&rent_hour=$rent_hour&PS_time=$PS_time&PE_time=$PE_time&cctv=$cctv'> Update </a> </td>";
                                         ?>
                                         <!-- <td class="column6"><a href="PGedit.php?id=<?php echo $id; ?>"class="btn btn-info" name="Edit">Edit</a></td> -->
                                         <?php
