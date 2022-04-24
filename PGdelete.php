83666c996a7636dc550a901ddb671309cd749a70
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-04-24 12:33:57 +0600

    Query executed & redirected to parking owner home

--- PGdelete.php
+++ PGdelete.php
@@ -2,7 +2,7 @@
     require_once('db_connect.php');
     $id = $_GET['id'];
     $queryapprove = "DELETE FROM parking_garage WHERE id = $id;";
-    //$approveExecute = mysqli_query($conn,$queryapprove);
-    //header("Location: admin.php");
+    $approveExecute = mysqli_query($conn,$queryapprove);
+    header("Location: POHome.php");
 
 ?>
