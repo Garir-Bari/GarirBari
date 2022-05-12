6f8679c272839371f7d47855c5560fad34f33cb2
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-13 00:58:45 +0600

    Updated info fields received by get method

--- PGupdateProcess.php
+++ PGupdateProcess.php
@@ -1,17 +1,17 @@
 <?php 
     require_once('db_connect.php');
 
-    // $PO_id = $_POST['PO_id'];
-    // //echo $PO_id;
-    // $c_name = $_POST['c_name'];
-    // $c_nid = $_POST['c_nid'];
-    // $phone = $_POST['phone'];
-    // $p_address = $_POST['p_address'];
-    // $no_of_spot = $_POST['no_of_spot'];
-    // $rent_hour = $_POST['rent_hour'];
-    // $PS_time = $_POST['PS_time'];
-    // $PE_time = $_POST['PE_time'];
-    // $cctv = $_POST['cctv'];
+     $id = $_POST['id'];
+    // echo $PO_id;
+     $c_name = $_POST['c_name'];
+     $c_nid = $_POST['c_nid'];
+     $phone = $_POST['phone'];
+     $p_address = $_POST['p_address'];
+     $no_of_spot = $_POST['no_of_spot'];
+     $rent_hour = $_POST['rent_hour'];
+     $PS_time = $_POST['PS_time'];
+     $PE_time = $_POST['PE_time'];
+     $cctv = $_POST['cctv'];
 
     
 
