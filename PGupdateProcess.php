695a3545d5776575c561be0c7b7a6986102f62d7
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-13 02:05:40 +0600

    After successfull update redirected to home

--- PGupdateProcess.php
+++ PGupdateProcess.php
@@ -2,7 +2,7 @@
     require_once('db_connect.php');
 
      $id = $_POST['id'];
-    // echo $PO_id;
+     //echo $cctv;
      $c_name = $_POST['c_name'];
      $c_nid = $_POST['c_nid'];
      $phone = $_POST['phone'];
@@ -12,28 +12,38 @@
      $PS_time = $_POST['PS_time'];
      $PE_time = $_POST['PE_time'];
      $cctv = $_POST['cctv'];
+     //echo $cctv;
 
-    
+    try{
+        $query = "UPDATE parking_garage SET c_name='$c_name', c_nid='$c_nid', contact='$phone', p_address='$p_address', no_of_spot='$no_of_spot', 
+               rent_hour='$rent_hour', PS_time='$PS_time', PE_time='$PE_time', cctv='$cctv' WHERE id= $id;";
+    }
+    catch(Exception $e) {
+        echo 'Message';
+    }
 
-     $query = "UPDATE parking_garage SET c_name=$c_name, c_nid=$c_nid, contact=$phone, p_address=$p_address, no_of_spot=$no_of_spot, 
-               rent_hour=$rent_hour, PS_time=$PS_time, PE_time=$PE_time, cctv=$cctv 
-               WHERE id= $id"
+    //  $query = "UPDATE parking_garage SET c_name=$c_name, c_nid=$c_nid, contact=$phone, p_address=$p_address, no_of_spot=$no_of_spot, 
+    //            rent_hour=$rent_hour, PS_time=$PS_time, PE_time=$PE_time, cctv=$cctv WHERE id= $id;";
      //(id, POid, c_name, c_nid, contact, p_address, no_of_spot, rent_hour, PS_time, PE_time, cctv , status) 
     // VALUES('', '$PO_id',  '$c_name', '$c_nid', '$phone', '$p_address', '$no_of_spot', '$rent_hour', '$PS_time', '$PE_time', '$cctv', 'pending');";
-
-    $execute_query = mysqli_query($conn,$query);
+    try{
+        $execute_query = mysqli_query($conn,$query);
+    }
+    catch(Exception $e) {
+        echo 'Message';
+    }
     
-    // if($execute_query){
-    //     ?>
-    //     <script>
-    //         window.location.assign('POHome.php');
-    //     </script>
-    //     <?php
-    // }
-    // else{
-    //     ///session doesn't contain any valid user email
-    //     echo 'unsucessful';
-    // }
+    if($execute_query){
+        ?>
+        <script>
+            window.location.assign('POHome.php');
+        </script>
+        <?php
+    }
+    else{
+        ///session doesn't contain any valid user email
+        echo 'unsucessful';
+    }
     
     
 ?>  
