c82f523bd622b1066eadfaf5fda7de7ba441499b
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-13 00:55:25 +0600

    DB connected for garage update

---
+++ PGupdateProcess.php
@@ -0,0 +1,36 @@
+<?php 
+    require_once('db_connect.php');
+
+    // $PO_id = $_POST['PO_id'];
+    // //echo $PO_id;
+    // $c_name = $_POST['c_name'];
+    // $c_nid = $_POST['c_nid'];
+    // $phone = $_POST['phone'];
+    // $p_address = $_POST['p_address'];
+    // $no_of_spot = $_POST['no_of_spot'];
+    // $rent_hour = $_POST['rent_hour'];
+    // $PS_time = $_POST['PS_time'];
+    // $PE_time = $_POST['PE_time'];
+    // $cctv = $_POST['cctv'];
+
+    
+
+    // $query = "INSERT INTO parking_garage (id, POid, c_name, c_nid, contact, p_address, no_of_spot, rent_hour, PS_time, PE_time, cctv , status) 
+    // VALUES('', '$PO_id',  '$c_name', '$c_nid', '$phone', '$p_address', '$no_of_spot', '$rent_hour', '$PS_time', '$PE_time', '$cctv', 'pending');";
+
+    // $execute_query = mysqli_query($conn,$query);
+    
+    // if($execute_query){
+    //     ?>
+    //     <script>
+    //         window.location.assign('POHome.php');
+    //     </script>
+    //     <?php
+    // }
+    // else{
+    //     ///session doesn't contain any valid user email
+    //     echo 'unsucessful';
+    // }
+    
+    
+?>  
