9d05a98e48324aa3e182ad562c90beaad3f12ff9
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-21 22:15:39 +0600

    Needed variable received by get method

--- COParkingProcess.php
+++ COParkingProcess.php
@@ -1,6 +1,8 @@
 <?php
     require_once('db_connect.php');
-    $id = $_GET['id'];
+    $Garage_id = $_GET['Garage_id'];
+    $available_spot = $_GET['available_spot'];
+    $Car_id = $_GET['Car_id'];
     $queryapprove = "DELETE FROM parking_garage WHERE id = $id;";
     $approveExecute = mysqli_query($conn,$queryapprove);
     header("Location: POHome.php");
