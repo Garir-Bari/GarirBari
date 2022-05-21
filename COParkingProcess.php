a78de3ffd537a513237258cfc8cd6c650dd4033a
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-21 22:13:28 +0600

    DB connected to paring process

---
+++ COParkingProcess.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('db_connect.php');
+    $id = $_GET['id'];
+    $queryapprove = "DELETE FROM parking_garage WHERE id = $id;";
+    $approveExecute = mysqli_query($conn,$queryapprove);
+    header("Location: POHome.php");
+
+?>
