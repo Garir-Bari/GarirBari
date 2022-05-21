b5fd12104fa98428003594d70266c73e5e3b6068
shahadat <mhossain173032@bscse.uiu.ac.bd>
2022-05-22 02:19:27 +0600

    Query Written for car checkout

--- COParkingCheckout.php
+++ COParkingCheckout.php
@@ -2,18 +2,18 @@
     require_once('db_connect.php');
 
     $id = $_GET['id'];
-    $$hour_rent = $_GET['$hour_rent'];
+    $hour_rent = $_GET['hour_rent'];
 
 
     $end_time = date("h:i:s");
 
-    $queryParking = "INSERT INTO parking_info (id, Cid, Gid, start_time, pdate,  status, hour_rent) VALUES('', '$Car_id',  '$Garage_id', '$s_time', '$date', 'Parked', '$rent_hour');";
-    $ParkingExecute = mysqli_query($conn,$queryParking);
+    $queryParking = "UPDATE parking_info SET end_time=$end_time, rent=$hour_rent, status='Completed';";
+    //$ParkingExecute = mysqli_query($conn,$queryParking);
     
     
-    if($ParkingExecute){
-        header("Location: COHome.php");
-    }
+    // if($ParkingExecute){
+    //     header("Location: COHome.php");
+    // }
     
 
 ?>
